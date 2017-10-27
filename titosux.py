55b2190e850e983ce411ce02f1550979ba7596a2
talhautsc <talha.khatri@mail.utoronto.ca>
2017-10-27 14:04:15 -0400

    changed line 2

--- titosux.py
+++ titosux.py
@@ -1,5 +1,5 @@
 def foo(int1, int2):
-    if int1 == 2: return 2*2
+    if int1 < 2: return 2*2
     elif int1 > 2: return 2*3
     elif int1 == 2 and int2 == 2: return 1
     if int2 >= 0: return 11
