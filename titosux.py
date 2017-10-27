0f9b28d51fbd6eb0baa4b054edf2293a0bdd8596
talhautsc <talha.khatri@mail.utoronto.ca>
2017-10-27 13:52:30 -0400

    added initial working copy of file

---
+++ titosux.py
@@ -0,0 +1,8 @@
+def foo(int1, int2):
+    if int1 == 2: return 2*2
+    elif int1 > 2: return 2*3
+    elif int1 == 2 and int2 == 2: return 1
+    if int2 >= 0: return 11
+    else: return int1*int2
+
+print(foo(5,4))
